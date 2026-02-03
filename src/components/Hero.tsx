35a2b38b6e1a241aff7075b0f6ebde63d2971ce2
Alexandre Albert Ndour <aa.ndour5@isepat.edu.sn>
2026-02-03 16:41:17 +0000

    Suppression d'un mot un peu incohérent

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -100,7 +100,6 @@ const Hero = () => {
                   </div>
                   <div>
                     <p className="text-sm font-semibold text-gray-700">Ans</p>
-                    <p className="text-xs text-gray-500">Ans d'expérience</p>
                   </div>
                 </div>
               </motion.div>
